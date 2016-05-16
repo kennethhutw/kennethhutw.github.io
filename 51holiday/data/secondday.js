480ba8e9ffb5e8f000d16cc08e12ee9720ef216b
Kennet hu <mfkkhu@hotmail.com>
2016-05-17 07:48:24 +0800

    initial version -51holiday

---
+++ 51holiday/data/secondday.js
@@ -0,0 +1,49 @@
+
+var seconddaylayer = {
+  "type": "FeatureCollection",
+  "generator": "overpass-turbo",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2014-05-04T04:09:02Z",
+  "features": [
+    {
+        "type": "Feature",
+        "id": "node/ddd",
+        "properties": {
+            "name": "KL International Airport",
+            "formatted_address": "Malaysia"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [101.7089291, 2.7512352]
+        },
+        "content":"<figure class='caption'><div class='image-container'><img src='http://localhost/example/Leaflet.MovingMarker-master/examples/images/secondday/4.png' style='width:100%;' /></div><figcaption>吉隆坡国际机场</figcaption></figure><p><span style='font-size:18px'>4/30要搭飞机出发，昆明我来啰!!</span></p><p><span style='font-size:18px'>5/3 晚上九点回到这，等11点的飞机回去新加坡了!! </span></p><p>&nbsp;</p>"
+    },
+    {
+        "type": "Feature",
+        "id": "node/ss",
+        "properties": {
+            "name": "昆明长水国际机场",
+            "formatted_address": "China"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [102.9327304, 25.1013443]
+        },
+        "content":"<figure class='caption'><div class='image-container'><img src='http://localhost/example/Leaflet.MovingMarker-master/examples/images/secondday/1.png' style='width:100%;'/></div><figcaption>昆明长水国际机场</figcaption></figure><p><span style='font-size:18px'>4/30 坐两个小时终于到昆明机场了，昆明我到了!!</span></p><p><span style='font-size:18px'>5/3 要回家了，掰掰昆明!</span></p><p>&nbsp;</p>"
+    },
+    {
+        "type": "Feature",
+        "id": "node/vvv",
+        "properties": {
+            "name": "Hostel",
+            "formatted_address": "China"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [102.7157993, 25.0439788]
+        },
+        "content":"<div class='image-container'><span style='font-size:18px'><img src='http://localhost/example/Leaflet.MovingMarker-master/examples/images/secondday/2.png' style='width:100%;' /></span></div><p>&nbsp;</p><p><span style='font-size:18px'>这是我后面三天睡觉的地方啰</span></p><p>&nbsp;</p><div class='image-container'><span style='font-size:18px'><img src='http://localhost/example/Leaflet.MovingMarker-master/examples/images/secondday/3.png' style='width:100%;' /></span></div><p>&nbsp;</p><p><span style='font-size:20px'>房子的主人是一对退休的夫妇，他们很用心的布置房子。</span></p><p>&nbsp;</p>"
+    }
+  ]
+};
+

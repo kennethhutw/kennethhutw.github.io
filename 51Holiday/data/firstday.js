a6307a1106e156542ea58f1bead9e2e5a86bfa29
Hu Chia Wei <kennethhutw@users.noreply.github.com>
2016-05-18 06:35:21 +0800

    addd

---
+++ 51Holiday/data/firstday.js
@@ -0,0 +1,52 @@
+
+
+var firstdaylayer = {
+  "type": "FeatureCollection",
+  "generator": "overpass-turbo",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2014-05-04T04:09:02Z",
+  "features": [
+    {
+        "type": "Feature",
+        "id": "node/ChIJY1uqz8irQjQRCVJuBqEowaQ",
+        "properties": {
+            "name": "Bugis  Singapore",
+            "formatted_address": "Singapore"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [103.8538013, 1.3010655]
+        },
+        "content" : "<p>&nbsp;</p><div class='image-container'><img  src='../examples/images/firstday/1.png' style='width:100%;' /></div><p>&nbsp;</p><h3><a href='http://place.qyer.com/poi/V2MJYFFvBzVTYA/' onmousedown='return rwt(this,'','','','2','AFQjCNHHyE9pntdeHGsHCwbsxtn9-WvU4A','4CH58TnqpJtJHhKU4SYaAg','0ahUKEwjvjMfL_NjMAhXLOo8KHbFCDK4QFggjMAE','','',event)' target='_blank'>武吉士街</a></h3><p>&nbsp;</p><p>Bugis是新加坡最热闹的逛吃逛吃好去处之一，相比于奢华&amp;游客众多的的乌节路，Bugis这边的商业街更加多元化，也更加时尚</p>"
+        
+    },
+    {
+        "type": "Feature",
+        "id": "node/ChIJY1uqz8irQjQRCVJuBqEowaQ",
+        "properties": {
+            "name": "JB Sentral",
+            "formatted_address": "Malaysia"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [103.7615745, 1.4618116]
+        },
+        "content" : "<p>&nbsp;</p><div class='image-container'>&nbsp;</div><div class='image-container'><img src='../examples/images/firstday/2.png' style='width:100%;'  /></div><p><span style='font-size:18px'><strong>新山中央車站&nbsp;</strong>從新加坡過海關到了馬來西亞的新山（马来语：<em>Johor Bahru</em>,&nbsp;<em>JB</em>，Bandar&nbsp;<em>Johor Bahru</em>）是<em>马来西亚</em>第二大城，准备要搭夜车去吉隆坡</span></p><p>&nbsp;</p><div class='image-container'><img src='../examples/images/firstday/3.png' style='width:100%;'  /></div><p>&nbsp;</p><p tabindex='0'><span style='font-size:18px'><strong>新山中央車站</strong>（<a href='https://zh.wikipedia.org/wiki/%E9%A9%AC%E6%9D%A5%E8%AF%AD' target='_blank' title='馬來語'>馬來語</a>：Johor Bahru Sentral；簡稱<strong>JB Sentral</strong>） 是位於<a href='https://zh.wikipedia.org/wiki/%E9%A9%AC%E6%9D%A5%E8%A5%BF%E4%BA%9A' target='_blank' title='馬來西亞'>馬來西亞</a><a href='https://zh.wikipedia.org/wiki/%E6%9F%94%E4%BD%9B%E5%B7%9E' target='_blank' title='柔佛州'>柔佛州</a><a href='https://zh.wikipedia.org/wiki/%E9%A6%96%E5%BA%9C' target='_blank' title='首府'>首府</a><a href='https://zh.wikipedia.org/wiki/%E6%96%B0%E5%B1%B1%E5%B8%82' target='_blank' title='新山市'>新山市</a>的火車站，功能類似於<a href='https://zh.wikipedia.org/wiki/%E5%90%89%E9%9A%86%E5%9D%A1%E4%B8%AD%E5%A4%AE%E8%BB%8A%E7%AB%99' target='_blank' title='吉隆坡中央車站'>吉隆坡中央車站</a>。新山中央車站繼承了已經關閉的<a href='https://zh.wikipedia.org/wiki/%E6%96%B0%E5%B1%B1%E7%81%AB%E8%BD%A6%E7%AB%99' target='_blank' title='新山火車站'>新山火車站</a>使命。新山中央車站總面積是79000平方公尺，接近<a href='https://zh.wikipedia.org/wiki/%E5%90%89%E9%9A%86%E5%9D%A1%E4%B8%AD%E5%A4%AE%E8%BB%8A%E7%AB%99' target='_blank' title='吉隆坡中央車站'>吉隆坡中央車站</a>（42000平方公尺）的兩倍。 新山中央車站在2010年10月21日開幕，並擁有一個巴士總站，巴士總站占地9500平方公尺。巴士總站預計是用可在1小時內容納15000人，以減緩使用已20年的<a href='https://zh.wikipedia.org/w/index.php?title=%E6%8B%89%E5%BA%86%E5%B7%B4%E5%A3%AB%E6%80%BB%E7%AB%99&amp;action=edit&amp;redlink=1' target='_blank' title='拉慶巴士總站（頁面不存在）'>拉慶巴士總站</a>的交通擁擠。 新山中央車站擁有2000個停車位。</span></p><p>&nbsp;</p><p>&nbsp;</p><p tabindex='0'><span style='font-size:18px'>本来想买卧铺的，可是在前一位竟然被卖光了 &gt;&lt;</span></p><p>&nbsp;</p>"
+    },
+    {
+        "type": "Feature",
+        "id": "node/ChIJY1uqz8irQjQRCVJuBqEowaQ",
+        "properties": {
+            "name": "吉隆坡",
+            "formatted_address": "Malaysia"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [101.5467663, 3.1393364]
+        },
+        "content":"<p>&nbsp;</p><figure class='caption'><div class='image-container'><img src='../examples/images/firstday/4.png' style='width:100%;' /></div><figcaption><span style='color:#000000'><span style='font-size:18px'>吉隆坡中央車站(KL Sentral)是吉隆坡的交通樞紐，也是機場往返於市中心的Aerobus和Skybus的終點站。KL Sentral包括了幾種出行工具，有往返於機場和市區的KLIA Express機場快線和 KLIA Transit機場電車線；連接吉隆坡同鄰國的曼穀和新加坡的KTM吉隆坡城際快線；連接吉隆坡和郊區城鎮的KTM Komuter公交列車線；貫通雙子塔、唐人街等各大景點的RapidKL輕軌電車(LRT)和Monorail單軌電車；還有各種巴士和出租車站。</span></span></figcaption></figure><p>&nbsp;</p><div class='image-container'><img src='../examples/images/firstday/5.png' style='width:100%;' /></div><p>&nbsp;</p><p><span style='font-size:16px'>一早到吉隆坡 KL Stenral 天亮了 大家都准备要出去玩&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></p><p>&nbsp;</p><p>&nbsp;</p><div class='image-container'><span style='font-size:16px'><img src='../examples/images/firstday/6.png' style='width:100%;' /></span></div><p><span style='font-size:18px'>车站内部里大家都走来走去</span></p><p>&nbsp;</p><p>&nbsp;</p><div class='image-container'><img src='../examples/images/firstday/7.png' style='width:100%;' /></div><p>&nbsp;</p><p><span style='font-size:20px'>吉隆坡的早餐，一次吃很辣吃不习惯</span></p><p>&nbsp;</p>"
+    }
+ 
+  ]
+};
+

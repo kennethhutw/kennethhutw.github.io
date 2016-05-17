aa17c1cef99a1649e8bd5525f341b8cc1439550d
Hu Chia Wei <kennethhutw@users.noreply.github.com>
2016-05-18 06:36:44 +0800

    Create fourthday.js

---
+++ 51Holiday/data/fourthday.js
@@ -0,0 +1,73 @@
+var fourthdaylayer = {
+  "type": "FeatureCollection",
+  "generator": "overpass-turbo",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2014-05-04T04:09:02Z",
+  "features": [
+     {
+        "type": "Feature",
+        "id": "node/ChIJY1uqz8irQjQRCVJuBqEowaQ",
+        "properties": {
+            "name": "Hostel",
+            "formatted_address": "China"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [102.7157993, 25.0439788]
+        },
+        "content":"<div class='image-container'><span style='font-size:18px'><img src='../examples/images/secondday/2.png' style='width:100%;' /></span></div><p>&nbsp;</p><p><span style='font-size:18px'>这是我后面三天睡觉的地方啰</span></p><p>&nbsp;</p><div class='image-container'><span style='font-size:18px'><img src='../examples/images/secondday/3.png' style='width:100%;' /></span></div><p>&nbsp;</p><p><span style='font-size:20px'>房子的主人是一对退休的夫妇，他们很用心的布置房子。</span></p><p>&nbsp;</p>"
+    },
+       {
+        "type": "Feature",
+        "id": "node/ChIJY1uqz8irQjQRCVJuBqEowaQ",
+        "properties": {
+            "name": "Cuihu Lake 翠湖",
+            "formatted_address": "China"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [102.7011452, 25.0513952]
+        },
+        "content":"<div class='image-container'><img  src='../examples/images/fourthday/1.png' style='width:100%;' /></div><p><span style='font-size:18px'>我住地方旁边的河，早起沿着跑步风景真棒</span></p><p>&nbsp;</p><div class='image-container'><img src='../examples/images/fourthday/2.png' style='width:100%;'  /></div><p>&nbsp;</p><div class='image-container'><img src='../examples/images/fourthday/3.png' style='width:100%;'  /></div><p>&nbsp;</p><p><span style='font-size:18px'>&nbsp;昨晚夜游昆明市看到的，真可爱</span></p><p>&nbsp;</p><div class='image-container'><img asrc='../examples/images/fourthday/4png' style='width:100%;'  /></div><p>&nbsp;</p><p><span style='font-size:18px'>昆明翠湖，原称&ldquo;菜海子&rdquo;，因湖东北有&ldquo;九泉所出，汇而成池&rdquo;，故又名&ldquo;九龙池&rdquo;。清康熙年间云贵总督范承勋、巡抚王继文于湖中建碧漪亭（俗称海心亭），水光潋滟，绿树成荫。唐继尧时在湖中筑有东西堤和南北堤，把湖一分为四，湖中有海心亭，西侧有观鱼堂，东南有水月轩。翠湖堤畔旧有&ldquo;十亩荷花鱼世界，半城杨柳抚楼台&rdquo;之联，被誉为&ldquo;城中碧玉&rdquo;。</span></p><p>&nbsp;</p><div class='image-container'><img src='../examples/images/fourthday/5.png' style='width:100%;'  /></div><p>&nbsp;</p><div class='image-container'><img src='../examples/images/fourthday/6.png' style='width:100%;'  /></div><p>&nbsp;</p><p><span style='font-size:18px'>翠湖真的很美，早上来这一边吃早餐一边看着翠湖风景。不過一早就很多人，应该是假日关系吧!!</span></p><div class='image-container'><img src='../examples/images/fourthday/7.png' style='width:100%;'  /></div><p>&nbsp;</p><p><span style='font-size:18px'>翠湖周围很多人在运动，这位老爷爷带着铁沙带绕着公园走，我见到第三次才偷偷拍到他。</span></p>"
+    },
+    {
+        "type": "Feature",
+        "id": "node/ChIJY1uqz8irQjQRCVJuBqEowaQ",
+        "properties": {
+            "name": "大观楼",
+            "formatted_address": "China"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [102.6737143, 25.0275218]
+        },
+        "content":"<p>&nbsp;</p><div class='image-container'><img src='../examples/images/fourthday/8.png' style='width:100%;'  /></div><p>&nbsp;</p><p>&nbsp;</p><p><span style='font-size:18px'>大觀樓坐落在滇池北濱，隔水與太華山相望，又稱「近華浦」，建築為中國傳統的四方三重檐木結構，琉璃翼角攢尖頂，始建於康熙三十五年（1696年），初為二層樓宇，逐漸成為「遠浦遙岑，風帆煙樹」的遊覽勝地。</span><br />&nbsp;</p><div class='image-container'><img  src='../examples/images/fourthday/9.png' style='width:100%;' /></div><p><span style='font-size:18px'>大观楼一个登上高楼，站在上面观天下的地方。</span></p><p>&nbsp;</p><p><span style='font-size:18px'>不过在参观时，看到游客不准规矩，跨过线还坐上古老的椅子上拍照，唉!! 我们国人的水平还是有待提升阿!!</span></p><p>&nbsp;</p><div class='image-container'><span style='font-size:18px'><img src='../examples/images/fourthday/10.png' style='width:100%;' /></span></div><p>&nbsp;<span style='font-size:18px'>绕一大圈从大观楼对面来看大观楼</span></p><p>&nbsp;</p>"
+    },
+    {
+        "type": "Feature",
+        "id": "node/ChIJY1uqz8irQjQRCVJuBqEowaQ",
+        "properties": {
+            "name": "海埂公园",
+            "formatted_address": "China"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [102.6634683, 24.9617588]
+        },
+        "content":"<div class='image-container'><img src='../examples/images/fourthday/11.png' style='width:100%;' /></div><p><span style='font-size:18px'>海埂公园建于六十年代初期，坐落在云南省&ldquo;四季如春&rdquo;的&ldquo;春城&rdquo;昆明市南郊，&ldquo;高原明珠&rdquo;滇池湖畔，距昆明市中心约10公里。东西长约2.5公里，南北宽约200米，占地750亩。</span><br />&nbsp;</p><div class='image-container'><div class='image-container'><img src='../examples/images/fourthday/12.png' style='width:100%;' /></div></div><p>&nbsp;</p><div class='image-container'><img src='../examples/images/fourthday/13.png' style='width:100%;' /></div><p><span style='font-size:18px'>逛逛着看到有人在演练求婚 ^^</span></p><p>&nbsp;</p><div class='image-container'><img src='../examples/images/fourthday/14.png' style='width:100%;' /></div><p>&nbsp;</p><p><span style='font-size:18px'>这里最特殊的应该就是这个了 搭直升机参观了 不过价格可是不亲民阿!!</span></p><p>&nbsp;</p><div class='image-container'><img src='../examples/images/fourthday/15.png' style='width:100%;' /></div><p>&nbsp;</p><p>&nbsp;</p>"
+    },
+    {
+        "type": "Feature",
+        "id": "node/ChIJY1uqz8irQjQRCVJuBqEowaQ",
+        "properties": {
+            "name": "南强街巷",
+            "formatted_address": "China"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [102.7083859, 25.0347297]
+        },
+        "content":"<div class='image-container'><img src='../examples/images/fourthday/16.png' style='width:100%;' /></div><p><span style='font-size:18px'>回到了城市里，开始无目的随意逛，目标寻宝啰</span></p><p>&nbsp;</p><div class='image-container'><img  src='../examples/images/fourthday/21.png' style='width:100%;'  /></div><p>&nbsp;</p><p><span style='font-size:18px'>在这里终于找到一间有特色的咖啡屋啰&nbsp; 在这小宅院里享受咖啡，人不多，但是人都在看书聊天，真是好地方阿&nbsp; </span></p><p>&nbsp;</p><div class='image-container'><span style='font-size:18px'><img  src='../examples/images/fourthday/17.png' style='width:100%;'  /></span></div><p>&nbsp;</p><div class='image-container'><img  src='../examples/images/fourthday/18.png' style='width:100%;'  /></div><p>&nbsp;</p><p><span style='font-size:18px'>休息好后走出巷口发现另一家店，虽然外表很古色古香的，但是里面很现代，当时想隔天要来这吃早餐，结果&hellip;&hellip;&hellip;&hellip;&hellip;没来 哈!!</span></p><p>&nbsp;</p><div class='image-container'><span style='font-size:18px'><img  src='../examples/images/fourthday/19.png' style='width:100%;'  /></span></div><p>&nbsp;</p><div class='image-container'><img  src='../examples/images/fourthday/20.png' style='width:100%;'  /></div><p>&nbsp;</p>"
+    }
+  ]
+};

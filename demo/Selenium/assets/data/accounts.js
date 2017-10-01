ee30f7f809fe8f2a6812b23e9ca5171090b97793
Kenneth <kenneth_hu@livemail.tw>
2017-10-01 13:42:47 +0800

    add Selenium

---
+++ demo/Selenium/assets/data/accounts.js
@@ -0,0 +1,10 @@
+var accounts = [{
+    account:"kenneth",
+    password:"password"
+}, {
+    account: "alan",
+    password: "123456"
+    }, {
+        account: "jack",
+        password: "123456"
+    }]
---
+++ demo/Selenium/assets/js/scripts.js
@@ -0,0 +1,104 @@
+
+jQuery(document).ready(function() {
+	$('.alert-danger').hide();
+	$('.alert-success').hide();
+    /*
+        Fullscreen background
+    */
+    $.backstretch("assets/img/backgrounds/1.jpg");
+    
+    /*
+        Login form validation
+    */
+    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+	$('#Signin').on('click', function(e) {
+    	var _countinue = true;
+		$('.login-form').find('input[type="text"], input[type="password"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+				$(this).addClass('input-error');
+				$('.alert-danger').show();
+				$('.alert-success').hide();
+				_countinue = false;
+				return;
+    		}
+    		else {
+				if (_countinue)
+					{
+					$(this).removeClass('input-error');
+					$('.alert-danger').hide();
+					}
+			
+    		}
+		});
+		
+		if (!_countinue)
+			return;
+		var _account = $("#username").val().toLowerCase();
+		var _password = $("#password").val().toLowerCase();
+		var _dashboard = $("#dashboard").is(':checked');
+		var validation =  accounts.filter(function (account) {
+			return (account.account == _account && account.password == _password);
+		});
+		if (validation.length >0){
+			$('.alert-success').show();
+			$('.alert-danger').hide();
+			if (_dashboard)
+				window.location.href = "dashboard.html?username=" + _account;
+		}
+		else
+		{
+				$('.alert-success').hide();
+				$('.alert-danger').show();
+		}
+		
+    });
+    
+    /*
+        Registration form validation
+    */
+    $('.registration-form input[type="text"], .registration-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+	$('#signup').on('click', function(e) {
+		var _countinue = true;
+		$(".registration-form").find('input[type="text"], input[type="radio"]').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+				$(this).addClass('input-error');
+				$('.alert-danger').show();
+				$('.alert-success').hide();
+				_countinue = false;
+				return;
+    		}
+    		else {
+				$(this).removeClass('input-error');
+				$('.alert alert-danger').hide();
+    		}
+		});
+		
+		if (!_countinue)
+			return;
+		var _account = $("#username").val().toLowerCase();
+		var _password = $("#password").val().toLowerCase();
+		var _dashboard = $("#dashboard").is(':checked');
+		
+		if (_account.length > 0 && _password.length  > 0) {
+			$('.alert-success').show();
+			$('.alert-danger').hide();
+			if (_dashboard)
+				window.location.href = "dashboard.html?username=" + _account;
+		}
+		else {
+			$('.alert-success').hide();
+			$('.alert-danger').show();
+		}
+    	
+    });
+    
+    
+});
